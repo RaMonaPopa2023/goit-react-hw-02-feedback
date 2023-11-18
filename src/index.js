2e6c9d7426bdf43006cb31330a4ac2862f71d7f5
RaMonaPopa2023 <128323291+RaMonaPopa2023@users.noreply.github.com>
2023-11-18 13:26:49 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
 import FeedbackWidget from 'components/FeedbackWidget';
 import './index.css';
 
